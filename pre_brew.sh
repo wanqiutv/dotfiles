212133c7374f4c544f67dce07a949b8769d58a5e
Bin Zhang <wanqiu.tv@gmail.com>
2017-10-29 03:36:02 +0800

    test

--- pre_brew.sh
+++
@@ -1,5 +0,0 @@
-#/usr/bin/env bash
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-sudo chgrp -R admin /usr/local
-sudo chmod -R g+w /usr/local
-brew cask install atom
