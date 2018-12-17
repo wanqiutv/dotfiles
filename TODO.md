8a7cbc2fc7bad1e3e60de4c37cf98604abfece7e
Bin Zhang <bin@autonomic.ai>
2018-12-17 11:42:36 +0800

    update

---
+++ TODO.md
@@ -0,0 +1,2 @@
+iterm 需要将key设置成natrual text edit 这样用option + arrow就能jump over word
+iterm 的appearance需要手动设置
--- shadow-shocks.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -o ~/Downloads/ShadowsocksX-NG.tar.gz https://codeload.github.com/shadowsocks/ShadowsocksX-NG/tar.gz/v1.8.2
